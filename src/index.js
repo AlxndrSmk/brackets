44f0763c842af2dc1c3e60208741aec697956c35
AlxndrSmk <voprosy262@gmail.com>
2022-08-12 23:45:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const stack = [];
+	const newBrackets = JSON.parse(JSON.stringify(bracketsConfig)).map((el) =>
+		el.reverse()
+	);
+	const map = Object.fromEntries(newBrackets);
+
+	for (let i = 0; i < str.length; i++) {
+		const current = str[i];
+
+		if (
+			Object.keys(map).includes(current) &&
+			stack[stack.length - 1] === map[current]
+		) {
+			stack.pop();
+		} else {
+			stack.push(current);
+		}
+	}
+	return stack.length === 0;
+};
